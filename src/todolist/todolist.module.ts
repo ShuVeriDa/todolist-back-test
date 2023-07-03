2ea139cb0f41fe2e8b5034b8ac5cab21d0b5badb
ShuVeriDa <bashtarov@outlook.com>
2023-07-03 22:19:29 +0300

    sss

---
+++ src/todolist/todolist.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TodolistController } from './todolist.controller';
+import { TodolistService } from './todolist.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodolistEntity } from './entity/todolist.entity';
+import { TasksEntity } from './entity/tasks.entity';
+import { UserEntity } from '../auth/entity/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([TodolistEntity, TasksEntity, UserEntity]),
+  ],
+  controllers: [TodolistController],
+  providers: [TodolistService],
+})
+export class TodolistModule {}
